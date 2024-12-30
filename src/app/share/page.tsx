3467fcf38b4f5b1fab83a3dec85eed14b0277905
uvenkatateja <118493739+uvenkatateja@users.noreply.github.com>
2024-12-30 22:14:32 +0530

    Update page.tsx

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -567,25 +567,6 @@ const Page = () => {
 
   return (
     <div className={styles.authpage}>
-      <button 
-        onClick={() => router.push('/myfiles')} 
-        className={styles.backButton}
-        title="Back to files"
-      >
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          fill="none" 
-          viewBox="0 0 24 24" 
-          strokeWidth={2.5} 
-          stroke="currentColor"
-        >
-          <path 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
-            d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" 
-          />
-        </svg>
-      </button>
       <div className={styles.authcontainer}>
         <div className={styles.inputcontaner}>
           <label htmlFor="email">Receiver&apos;s email</label>
