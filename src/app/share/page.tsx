6f4afa77548a85728895029855d14d91112ffe15
uvenkatateja <118493739+uvenkatateja@users.noreply.github.com>
2024-12-30 22:07:28 +0530

    Update page.tsx

--- src/app/share/page.tsx
+++ src/app/share/page.tsx
@@ -570,12 +570,13 @@ const Page = () => {
       <button 
         onClick={() => router.push('/myfiles')} 
         className={styles.backButton}
+        title="Back to files"
       >
         <svg 
           xmlns="http://www.w3.org/2000/svg" 
           fill="none" 
           viewBox="0 0 24 24" 
-          strokeWidth={2} 
+          strokeWidth={2.5} 
           stroke="currentColor"
         >
           <path 
@@ -584,7 +585,6 @@ const Page = () => {
             d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" 
           />
         </svg>
-        Back
       </button>
       <div className={styles.authcontainer}>
         <div className={styles.inputcontaner}>
